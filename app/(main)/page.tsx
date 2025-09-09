4c5c905c9b684d24fabbcd5e9b2044e6fd338c57
miankks <bilal.jan085@gmail.com>
2025-09-09 16:11:34 +0200

    create signin sinup UI

---
+++ app/(main)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1 className='text-2xl text-slate-700'>Home page</h1>
+      <Button>Hi</Button>
+    </div>
+  )
+}
+
+export default HomePage
